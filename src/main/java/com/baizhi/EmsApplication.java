fb318e386bf8a5574170aa95efbea6b4f5689340
github-lilong <47850036+github-lilong@users.noreply.github.com>
2019-02-21 19:41:04 +0800

    Update EmsApplication.java

--- src/main/java/com/baizhi/EmsApplication.java
+++ src/main/java/com/baizhi/EmsApplication.java
@@ -5,7 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @MapperScan("com.baizhi.dao")
 public class EmsApplication {
-
+    
+    //This is test
+    //测试是否有乱码
+    
+    
+    
     public static void main(String[] args) {
         SpringApplication.run(EmsApplication.class, args);
     }
